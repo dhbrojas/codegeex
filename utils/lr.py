8d2a84ebbdbf25066f06c7c601a04ec95f3ee8cd
Diego ROJAS <rojasdiegopro@gmail.com>
2024-05-07 17:00:21 +0800

    Fix WSD learning rate scheduler implementation

--- utils/lr.py
+++ utils/lr.py
@@ -1,13 +1,16 @@
-def wsd_learning_rate_scheduler(W, T, eta, decay_rate=0.95, decay_step=1):
+import math
+
+
+def wsd_learning_rate_scheduler(W, S, D, min_lr_scale_factor=0.1):
     """
-    Returns a lambda function for PyTorch's LambdaLR scheduler implementing the WSD learning rate schedule.
+    Returns a lambda function for PyTorch's LambdaLR scheduler implementing the
+    WSD learning rate schedule.
 
     Parameters:
     - W: The last step of the warmup phase.
-    - T: The last step of the stable phase.
-    - eta: The maximum learning rate (used during the stable phase).
-    - decay_rate: The decay rate of the learning rate for each decay_step after T.
-    - decay_step: The step interval for applying the decay_rate.
+    - S: The last step of the stable phase.
+    - D: The last step of the decay phase.
+    - min_lr_scale_factor: The minimum learning rate is eta * min_lr_scale_factor.
 
     Returns:
     - A lambda function to be used with torch.optim.lr_scheduler.LambdaLR.
@@ -15,12 +18,15 @@ def wsd_learning_rate_scheduler(W, T, eta, decay_rate=0.95, decay_step=1):
 
     def lr_lambda(current_step):
         if current_step <= W:
-            return (current_step / W) * eta
-        elif current_step <= T:
-            return eta
+            return current_step / W
+        elif current_step <= S:
+            return 1
         else:
-            steps_into_decay = current_step - T
-            decay_factor = decay_rate ** (steps_into_decay / decay_step)
-            return eta * decay_factor
+            return (
+                min_lr_scale_factor
+                + (1 - min_lr_scale_factor)
+                * (1 + math.cos(math.pi * (current_step - S) / (D - S)))
+                / 2
+            )
 
     return lr_lambda
